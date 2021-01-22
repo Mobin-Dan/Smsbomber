b0ee603d3c98290fc0d37b05e4491cbaefb85b7c
Mobin-Dan <68151748+Mobin-Dan@users.noreply.github.com>
2021-01-22 13:19:43 +0330

    Update and rename smsbomber.py to install.py

---
+++ install.py
@@ -0,0 +1,8 @@
+import os
+os.system("pip install colorama")
+os.system("pip install time")
+os.system("pip install socket")
+os.system("pip install time")
+os.system("unzip dist.zip")
+os.system("cd dist")
+os.system("python sms.py")
