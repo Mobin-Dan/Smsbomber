d832d8baed31c6ce7bf33f94b1f44a8ab3df440c
Mobin-Dan <68151748+Mobin-Dan@users.noreply.github.com>
2021-03-08 10:20:26 +0330

    Delete install.py

--- install.py
+++
@@ -1,8 +0,0 @@
-import os
-os.system("pip install colorama")
-os.system("pip install time")
-os.system("pip install socket")
-os.system("pip install requests")
-os.system("unzip sms.zip")
-os.system("cd sms")
-os.system("python spamer.py")
